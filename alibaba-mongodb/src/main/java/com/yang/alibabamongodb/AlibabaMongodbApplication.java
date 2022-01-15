0bf40ba5485631017b5b76efbd5e6c113071e7df
JaxYoun <gao88jie@qq.com>
2022-01-16 01:47:17 +0800

    新加入mongodb微服务，暂时是单体的

---
+++ alibaba-mongodb/src/main/java/com/yang/alibabamongodb/AlibabaMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.yang.alibabamongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlibabaMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlibabaMongodbApplication.class, args);
+    }
+
+}
---
+++ alibaba-mongodb/src/test/java/com/yang/alibabamongodb/AlibabaMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yang.alibabamongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlibabaMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
