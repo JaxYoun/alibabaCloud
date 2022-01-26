7dca1b8e959074b39b7bf9dcfab8742f2cde6f62
JaxYoun <gao88jie@qq.com>
2022-01-26 18:22:41 +0800

    集成swagger2

--- alibaba-mysql/src/main/java/com/yang/alibabamysql/controller/UserController.java
+++ alibaba-mysql/src/main/java/com/yang/alibabamysql/controller/UserController.java
@@ -5,12 +5,18 @@ import com.yang.alibabamysql.domain.po.UserPO;
 import com.yang.alibabamysql.domain.vo.UserVO;
 import com.yang.alibabamysql.service.UserService;
 import comg.yang.alibabacommon.entity.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
+@Api(value = "用户接口", tags = "用户接口 yjx")
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -18,12 +24,16 @@ public class UserController {
     @Resource
     private UserService userService;
 
-    @GetMapping("/select")
-    public Result<UserVO> select() {
-        Long id = 1L;
+    @ApiOperation("查询单个")
+    @GetMapping("/one")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "主键", value = "id", required = true)
+    })
+    public Result<UserVO> one(@RequestParam Long id) {
         return Result.success(this.userService.select(id));
     }
 
+    @ApiOperation("分页查询")
     @GetMapping("/page")
     public Result<IPage<UserPO>> page() {
         return Result.success(this.userService.page());
--- alibaba-mysql/src/main/java/com/yang/alibabamysql/domain/vo/UserVO.java
+++ alibaba-mysql/src/main/java/com/yang/alibabamysql/domain/vo/UserVO.java
@@ -1,12 +1,17 @@
 package com.yang.alibabamysql.domain.vo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
+@ApiModel("用户VO")
 public class UserVO {
 
+    @ApiModelProperty("主键")
     private Long id;
 
+    @ApiModelProperty("名称")
     private String name;
 
     public Long getId() {
