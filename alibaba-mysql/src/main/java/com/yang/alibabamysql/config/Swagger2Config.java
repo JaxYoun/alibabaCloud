1bb76d3220489e4e2fb7cf7651ab6ceb5b4a83ff
JaxYoun <gao88jie@qq.com>
2022-01-27 10:24:24 +0800

    集成swagger3，集成失败【Error creating bean with name 'swagger2ControllerMapping'】

---
+++ alibaba-mysql/src/main/java/com/yang/alibabamysql/config/Swagger2Config.java
@@ -0,0 +1,21 @@
+package com.yang.alibabamysql.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)  //1.指定Swagger版本
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.yang.alibabamysql.controller"))  //2.指定Controller包名
+                .build();
+    }
+}
--- alibaba-mysql/src/main/java/com/yang/alibabamysql/config/Swagger3Config.java
+++ alibaba-mysql/src/main/java/com/yang/alibabamysql/config/Swagger3Config.java
@@ -1,22 +1,46 @@
-//package com.yang.alibabamysql.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.oas.annotations.EnableOpenApi;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//
-//@Configuration
-//@EnableOpenApi
-//public class Swagger3Config {
-//
-//    @Bean
-//    public Docket createRestApi() {
-//        return new Docket(DocumentationType.OAS_30)  //1.指定Swagger版本
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("com.yang.alibabamysql.controller"))  //2.指定Controller包名
-//                .build();
-//    }
-//
-//}
+package com.yang.alibabamysql.config;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableOpenApi
+public class Swagger3Config {
+
+    /*@Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.OAS_30)  //1.指定Swagger版本
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.yang.alibabamysql.controller"))  //2.指定Controller包名
+                .build();
+    }*/
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Swagger3接口文档")
+                .description("更多请咨询服务开发者Ray。")
+                .contact(new Contact("Ray。", "http://www.ruiyeclub.cn", "ruiyeclub@foxmail.com"))
+                .version("1.0")
+                .build();
+    }
+
+}
